c3a5eae6eecb0f846acce1ea7565743f7e6aea16
Dmitry Bychenko <Zwerg.CopperNickel@gmail.com>
2021-03-12 17:00:27 +0300

    Dump (start)

---
+++ Literatu/Literatu.Dump.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Literatu {
+
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// Text Dump
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public static class TextDump {
+    #region Public
+
+    //-----------------------------------------------------------------------------------------------------------------
+    //
+    /// <summary>
+    /// Dump 
+    /// </summary>
+    //
+    //-----------------------------------------------------------------------------------------------------------------
+
+    public static string Dump(this char value) {
+      var category = char.GetUnicodeCategory(value);
+
+      string result = string.Join(" ",
+        $"{(value.IsVisible() ? "'{value}'": "[invisible]")}",
+        $"(\\u{((int) value):x4})",
+        $"{category}"
+      );
+
+      return result;
+    }
+
+    #endregion Public
+  }
+
+}
---
+++ Literatu/Literatu.Unicodes.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Literatu {
+  
+  public static class Unicode {
+    #region Private Data
+
+    private static HashSet<UnicodeCategory> s_Invisible = new() {
+      UnicodeCategory.NonSpacingMark,
+      UnicodeCategory.Control,
+      UnicodeCategory.ModifierLetter,
+      UnicodeCategory.LineSeparator,
+      UnicodeCategory.ModifierSymbol,
+      UnicodeCategory.ParagraphSeparator,
+      UnicodeCategory.SpacingCombiningMark,
+      UnicodeCategory.Surrogate,
+    };
+
+    #endregion Private Data
+
+    #region Public
+
+    /// <summary>
+    /// Is Visible Category
+    /// </summary>
+    public static bool IsVisible(this UnicodeCategory category) =>
+      !s_Invisible.Contains(category);
+
+    /// <summary>
+    /// Is Visible Character
+    /// </summary>
+    public static bool IsVisible(this char value) =>
+      !s_Invisible.Contains(char.GetUnicodeCategory(value));
+
+    #endregion Public
+  }
+
+}
