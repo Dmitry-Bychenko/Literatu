b3b754216d2914a21cc49df48598d70497220265
Dmitry Bychenko <Zwerg.CopperNickel@gmail.com>
2021-08-05 11:56:49 +0300

    IndexesOf

---
+++ Literatu/Linq/Literatu.Linq.Enumerations.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+
+namespace Literatu.Linq {
+  
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// Indexes, Findings
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public static partial class StringExtensions {
+    #region Public
+
+    /// <summary>
+    /// Indexes Of
+    /// </summary>
+    public static IEnumerable<int> IndexesOf(this string source, string value, int startIndex) {
+      if (string.IsNullOrEmpty(source) || string.IsNullOrEmpty(value))
+        yield break;
+
+      for (int index = source.IndexOf(value, startIndex); 
+               index >= 0; 
+               index = source.IndexOf(value, ++index)) 
+        yield return index;
+    }
+
+    /// <summary>
+    /// Indexes Of
+    /// </summary>
+    public static IEnumerable<int> IndexesOf(this string source, string value) =>
+      IndexesOf(source, value, 0);
+
+    /// <summary>
+    /// Last Indexes Of
+    /// </summary>
+    public static IEnumerable<int> LastIndexesOf(this string source, string value, int startIndex) {
+      if (string.IsNullOrEmpty(source) || string.IsNullOrEmpty(value))
+        yield break;
+      
+      for (int index = source.LastIndexOf(value, startIndex);
+               index >= 0;
+               index = index == 0 ? -1 : source.LastIndexOf(value, --index))
+        yield return index;
+    }
+
+    /// <summary>
+    /// Last Indexes Of
+    /// </summary>
+    public static IEnumerable<int> LastIndexesOf(this string source, string value) =>
+      LastIndexesOf(source, value, source is null ? 0 : source.Length - 1);
+
+    #endregion Public
+  }
+
+}
